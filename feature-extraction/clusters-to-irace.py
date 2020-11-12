2aab464bdcf5d7e6a511edc28e5c65aa7f40c924
Leonardo Holtz <leo.holtz98@gmail.com>
2020-11-12 15:33:27 -0300

    Add files via upload
    It creates a file for each cluster

---
+++ feature-extraction/clusters-to-irace.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+clusters_name_file = sys.argv[1]
+clusters_file = open(clusters_name_file, "r")
+current_cluster = None
+for line in clusters_file:
+	line = line.split(",")
+	if current_cluster == None or line[1] != current_cluster:
+		if current_cluster != None:
+			current_cluster_file.close()
+		current_cluster = line[1]
+		current_cluster_file = open("cluster" + str(current_cluster) + ".txt", "a")
+		current_cluster_file.write(line[0] + "\n")
+	else:
+		current_cluster_file.write(line[0] + "\n")
+
+current_cluster_file.close()
+clusters_file.close()
