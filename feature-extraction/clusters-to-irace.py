f42b1c5a7d9f42468c26ffded8bb7239fc14eba2
Leonardo Holtz <leo.holtz98@gmail.com>
2020-11-12 16:42:08 -0300

    fix newline in the end of each line

--- feature-extraction/clusters-to-irace.py
+++ feature-extraction/clusters-to-irace.py
@@ -7,11 +7,12 @@ clusters_file = open(clusters_name_file, "r")
 current_cluster = None
 for line in clusters_file:
 	line = line.split(",")
+	line[1] = line[1][:-1]
 	if current_cluster == None or line[1] != current_cluster:
 		if current_cluster != None:
 			current_cluster_file.close()
 		current_cluster = line[1]
-		current_cluster_file = open("cluster" + str(current_cluster) + ".txt", "a")
+		current_cluster_file = open("cluster" + current_cluster + ".txt", "a")
 		current_cluster_file.write(line[0] + "\n")
 	else:
 		current_cluster_file.write(line[0] + "\n")
